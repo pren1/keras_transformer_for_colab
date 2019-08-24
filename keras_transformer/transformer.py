216ddc8d7cd937fb8d9405951ab769525b17b381
pren1 <33309499+pren1@users.noreply.github.com>
2019-08-23 17:07:25 -0700

    Add files via upload

--- keras_transformer/transformer.py
+++ keras_transformer/transformer.py
@@ -9,6 +9,7 @@ import pdb
 import tensorflow.keras as keras
 import tensorflow
 
+
 __all__ = [
     'get_custom_objects', 'get_encoders', 'get_decoders', 'get_model', 'decode',
     'attention_builder', 'feed_forward_builder', 'get_encoder_component', 'get_decoder_component',
@@ -21,8 +22,8 @@ def get_custom_objects():
         'MultiHeadAttention': MultiHeadAttention,
         'FeedForward': FeedForward,
         'TrigPosEmbedding': TrigPosEmbedding,
-        # 'EmbeddingRet': EmbeddingRet,
-        # 'EmbeddingSim': EmbeddingSim,
+        'EmbeddingRet': EmbeddingRet,
+        'EmbeddingSim': EmbeddingSim,
     }
 
 
@@ -402,8 +403,6 @@ def get_model(token_num,
         encoder_embed_weights = [encoder_embed_weights]
     if decoder_embed_weights is not None:
         decoder_embed_weights = [decoder_embed_weights]
-    encoder_embed_weights = encoder_embed_weights[0]
-    decoder_embed_weights = decoder_embed_weights[0]
 
     if not isinstance(embed_trainable, list):
         embed_trainable = [embed_trainable, embed_trainable]
@@ -414,33 +413,36 @@ def get_model(token_num,
         decoder_embed_trainable = decoder_embed_weights is None
 
     if use_same_embed:
-        encoder_embed_layer = decoder_embed_layer = keras.layers.Embedding(
+        encoder_embed_layer = decoder_embed_layer = EmbeddingRet(
             input_dim=encoder_token_num,
             output_dim=embed_dim,
-            embeddings_initializer = tensorflow.keras.initializers.Constant(encoder_embed_weights),
-            trainable=encoder_embed_trainable,
             mask_zero=False,
-            name='Token-Embedding',)
+            weights=encoder_embed_weights,
+            trainable=encoder_embed_trainable,
+            name='Token-Embedding',
+        )
     else:
-        encoder_embed_layer = keras.layers.Embedding(
+        encoder_embed_layer = EmbeddingRet(
             input_dim=encoder_token_num,
             output_dim=embed_dim,
-            embeddings_initializer = tensorflow.keras.initializers.Constant(encoder_embed_weights),
-            trainable=encoder_embed_trainable,
             mask_zero=False,
-            name='Encoder-Token-Embedding',)
-        decoder_embed_layer = keras.layers.Embedding(
+            weights=encoder_embed_weights,
+            trainable=encoder_embed_trainable,
+            name='Encoder-Token-Embedding',
+        )
+        decoder_embed_layer = EmbeddingRet(
             input_dim=decoder_token_num,
             output_dim=embed_dim,
-            embeddings_initializer = tensorflow.keras.initializers.Constant(decoder_embed_weights),
-            trainable=decoder_embed_trainable,
             mask_zero=False,
-            name='Decoder-Token-Embedding',)
+            weights=decoder_embed_weights,
+            trainable=decoder_embed_trainable,
+            name='Decoder-Token-Embedding',
+        )
     encoder_input = keras.layers.Input(shape=(None,), name='Encoder-Input')
     encoder_embed = TrigPosEmbedding(
         mode=TrigPosEmbedding.MODE_ADD,
         name='Encoder-Embedding',
-    )(encoder_embed_layer(encoder_input))
+    )(encoder_embed_layer(encoder_input)[0])
     encoded_layer = get_encoders(
         encoder_num=encoder_num,
         input_layer=encoder_embed,
@@ -455,9 +457,7 @@ def get_model(token_num,
         adapter_activation=adapter_activation,
     )
     decoder_input = keras.layers.Input(shape=(None,), name='Decoder-Input')
-    decoder_embed  = decoder_embed_layer(decoder_input)
-    # decoder_embed, decoder_embed_weights = decoder_embed_layer(decoder_input)
-
+    decoder_embed, decoder_embed_weights = decoder_embed_layer(decoder_input)
     decoder_embed = TrigPosEmbedding(
         mode=TrigPosEmbedding.MODE_ADD,
         name='Decoder-Embedding',
@@ -476,15 +476,12 @@ def get_model(token_num,
         adapter_units=adapter_units,
         adapter_activation=adapter_activation,
     )
-    # dense_layer_1 = keras.layers.TimeDistributed(keras.layers.Dense(embed_dim*4, activation='tanh'))
-    predicted_char_layer = keras.layers.TimeDistributed(keras.layers.Dense(decoder_token_num, activation='softmax', name='Output'))
-    # dense_layer_output_1 = dense_layer_1(decoded_layer)
-    dense_layer = predicted_char_layer(decoded_layer)
-    # dense_layer = EmbeddingSim(
-    #     trainable=trainable,
-    #     name='Output',
-    # )([decoded_layer, decoder_embed_weights])
+    dense_layer = EmbeddingSim(
+        trainable=trainable,
+        name='Output',
+    )([decoded_layer, decoder_embed_weights])
     return keras.models.Model(inputs=[encoder_input, decoder_input], outputs=dense_layer)
+
 
 def _get_max_suffix_repeat_times(tokens, max_len):
     detect_len = min(max_len, len(tokens))
@@ -545,9 +542,8 @@ def decode(model,
                 batch_inputs.append(tokens[i][:])
                 batch_outputs.append(decoder_inputs[i])
                 max_input_len = max(max_input_len, len(tokens[i]))
-        'Do not use pad tokens here'
-        # for i in range(len(batch_inputs)):
-        #     batch_inputs[i] += [pad_token] * (max_input_len - len(batch_inputs[i]))
+        for i in range(len(batch_inputs)):
+            batch_inputs[i] += [pad_token] * (max_input_len - len(batch_inputs[i]))
         predicts = model.predict([np.array(batch_inputs), np.array(batch_outputs)])
         for i in range(len(predicts)):
             if top_k == 1:
